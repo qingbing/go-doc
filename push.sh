999577b3520f28f9a2b6c6e0daed1b51628d3c3e
duqingbing <duqingbing@ghostcloud.cn>
2022-11-13 02:06:18 +0800

    202211130206

--- push.sh
+++ push.sh
@@ -8,15 +8,15 @@ CUR_BRANCH=$(git branch | grep '\*' | awk -F ' ' '{print $2}')
 # get the release comment
 commitMsg=$1
 # check comment
-if [[ ! -n $commitMsg ]]; then
-	commitMsg=$(date "+%Y${dateExp}%m${dateExp}%d${exp}%H${secExp}%M")
+if [[ -z $commitMsg ]]; then
+	commitMsg=$(date "+%Y%m%d%H%M")
 fi
 
 # commit the comment
-git commit -m $commitMsg
+git commit -m "${commitMsg}"
 
 # push
-git push origin $CUR_BRANCH
+git push origin "${CUR_BRANCH}"
 # tip
 if [[ $? -ne 0 ]]; then
 	echo "push fail"
